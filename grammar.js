a10aa4932bcfe6d314cd523812210dc8afa109b3
Dietrich Daroch <Dietrich.Daroch@gmail.com>
2024-03-10 01:46:39 -0800

    WIP: Update grammar

--- grammar.js
+++ grammar.js
@@ -1,9 +1,16 @@
 module.exports = grammar ({
-  name: "ezML",
+	name: "ezML",
 
-  rules: {
-    // TODO: Add the grammar rules
-    source_file: $ => 'hello',
-    verbatim_line: $ => '<!DOCTYPE html>'
-  }
+	rules: {
+		// TODO: Add the grammar rules
+		source_file: $ => 'hello',
+
+
+		identifier: _ => /[_\p{XID_Start}][_\p{XID_Continue}]*/,
+
+		// Comments
+		verbatim_comment: _ => token(seq('<!', /.*/, '>')),
+		loud_comment:     _ => token(seq('/', /.*/)),
+		silent_comment:   _ => token(seq('#', /.*/))
+	}
 });
