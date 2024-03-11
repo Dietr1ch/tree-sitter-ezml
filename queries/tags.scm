f25b858d9dc94e3d4f39f0bbd28153a95ab3732c
Dietrich Daroch <Dietrich.Daroch@gmail.com>
2024-03-11 00:59:39 -0700

    WIP: Add mock support for tags

---
+++ queries/tags.scm
@@ -0,0 +1,18 @@
+
+(element
+  (element_name) @definition.class)
+
+(element
+  (element_name)
+  (element_id) @name) @definition.id
+
+(element
+  (element_name)
+  (tag
+   (tag_name) @name)) @definition.tag
+
+(element
+  (element_name)
+  (attribute
+   (attribute_name) @name
+   (attribute_value) @definition.value)) @definition.attribute
