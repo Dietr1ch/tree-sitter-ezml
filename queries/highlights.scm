be04bf22dd8d27a1bf06ec153acccebbd2d69096
Dietrich Daroch <Dietrich.Daroch@gmail.com>
2024-03-11 00:00:01 -0700

    WIP: Highlighting

---
+++ queries/highlights.scm
@@ -0,0 +1,15 @@
+; https://tree-sitter.github.io/tree-sitter/syntax-highlighting
+
+(doctype) @module
+
+(loud_comment) @comment
+(silent_comment) @comment2
+
+
+(element_name) @function
+(element_id) @function.method
+
+(tag_name) @type
+
+(attribute_name) @attribute
+(attribute_value) @string
